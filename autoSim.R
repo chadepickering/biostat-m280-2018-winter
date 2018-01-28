7c7cfad37d2a18470b799cc7057e73f42e8a1d2d
cepickering <cepickering@g.ucla.edu>
2018-01-28 18:31:17 +0000

    Call function at end of runSim with the fields left as the argument names. Added character strings at the top of runSim to make autoSim properly automate the 15 files.

--- autoSim.R
+++ autoSim.R
@@ -1,15 +1,14 @@
 # autoSim.R
-
-nVals = seq(100, 500, by=100)
-dist = c("gaussian", "t1", "t5")
+nVals <- seq(100, 500, by=100)
+dist <- c("gaussian", "t1", "t5")
 for (i in 1:3) {
   for (n in nVals) {
-    oFile = paste(dist[i], '_n', n, '.txt', sep='')
-    arg = paste('n=', n, sep='')
-    sysCall = paste('nohup Rscript runSim.R seed=280 ', arg, ' dist="', 
+    oFile <- paste(dist[i], '_n', n, '.txt', sep='')
+    arg <- paste('n=', n, sep='')
+    sysCall <- paste('nohup Rscript runSim.R seed=280 ', arg, ' dist="', 
                     dist[i], '" rep=50', " > ", oFile, sep='')
-    system(sysCall)
-    print(paste("sysCall=", sysCall, sep=""))
+    system(sysCall, ignore.stderr = TRUE)
+    #print(paste("sysCall=", sysCall, sep=""))
     print(sysCall)
   }
 }
--- runSim.R
+++ runSim.R
@@ -1,10 +1,14 @@
+gaussian = "gaussian"
+t1 = "t1"
+t5 = "t5"
+
 ## parsing command arguments
 for (arg in commandArgs(TRUE)) {
-  eval(parse(text=arg))
+  eval(parse(text = arg))
 }
 
 ## check if a given integer is prime
-isPrime = function(n) {
+isPrime <- function(n) {
   if (n <= 3) {
     return (TRUE)
   }
@@ -15,9 +19,9 @@ isPrime = function(n) {
 }
 
 ## estimate mean only using observation with prime indices
-estMeanPrimes = function(x) {
-  n = length(x)
-  ind = sapply(1:n, isPrime)
+estMeanPrimes <- function(x) {
+  n <- length(x)
+  ind <- sapply(1:n, isPrime)
   return (mean(x[ind]))
 }
 
@@ -27,21 +31,21 @@ avgMSE <- function(seed, n, dist, rep){
   classical_vector <- c()
   
   for(i in 1:rep){
-    if (dist == "gaussian"){
+    if (dist == gaussian){
       x <- rnorm(n)
-    } else if (dist == "t1"){ # Cauchy distribution has no mean!
+    } else if (dist == t1){ # Cauchy distribution has no mean!
       x <- rt(n, 1)
-    } else if (dist == "t5"){
+    } else if (dist == t5){
       x <- rt(n, 5)
     }
     
-    prime_vector[i] <- (estMeanPrimes(x)-0)^2
-    classical_vector[i] <- (mean(x)-0)^2
+    prime_vector[i] <- (estMeanPrimes(x) - 0)^2
+    classical_vector[i] <- (mean(x) - 0)^2
   }
   
   prime_mse <- sum(prime_vector)/rep
   classical_mse <- sum(classical_vector)/rep
   return(c(prime_mse, classical_mse))
-
 }
 
+avgMSE(seed, n, dist, rep)
